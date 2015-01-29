95bc64a0b42e66425935746b8a3257450aa5fee2
Andre Kibbe <akibbe02@gmail.com>
2015-01-28 21:50:49 -0800

    Introduce visualization functionality

---
+++ app/assets/javascripts/graph.js
@@ -0,0 +1,15 @@
+var Graph = function (selector, data, kind) {
+  this.selector = selector;
+  this.data = data;
+  this.kind = kind;
+};
+
+Graph.prototype.getData = function() {
+
+};
+
+Graph.prototype.render = function() {
+
+}
+
+Graph.instances = [];
