d9bdcde894b47e21725b499af17caea7d13e57d5
Andre Kibbe <akibbe02@gmail.com>
2015-01-29 13:15:20 -0800

    Add answers-per-question method to poll serializer

--- app/lib/poll_serializer.rb
+++ app/lib/poll_serializer.rb
@@ -15,5 +15,23 @@ class PollSerializer
         scale: [0, data.max + 1]
       }
     }
+
+    def self.answers_per_question question
+      answers_per_question = question.answers.group_by(&:possible_answer)
+      data = answers_per_question.map { |possible_answer, answers| answers.length }
+      series = answers_per_question.map { |possible_answer, answers| possible_answer.title }
+      {
+        data: data,
+        title: "Polls answered by month",
+        x_axis: {
+          legend: "Polls per month",
+          series: series
+        },
+        y_axis: {
+          legend: "No. polls",
+          scale: [0, data.max + 1]
+        }
+      }
+    end
   end
 end
